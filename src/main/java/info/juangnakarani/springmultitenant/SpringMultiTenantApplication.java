116d14ac14123fd92dc948552a05c1dd00ac3368
juangnakarani <juang.nakarani@gmail.com>
2024-04-03 12:46:39 +0700

    first commit

---
+++ src/main/java/info/juangnakarani/springmultitenant/SpringMultiTenantApplication.java
@@ -0,0 +1,13 @@
+package info.juangnakarani.springmultitenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiTenantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultiTenantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/juangnakarani/springmultitenant/SpringMultiTenantApplicationTests.java
@@ -0,0 +1,13 @@
+package info.juangnakarani.springmultitenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiTenantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
