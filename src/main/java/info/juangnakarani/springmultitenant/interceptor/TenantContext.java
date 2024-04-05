1d4ce50b953facf66292d287566e52d25d83e78b
juangnakarani <juang.nakarani@gmail.com>
2024-04-05 22:43:33 +0700

    update tenant context

--- src/main/java/info/juangnakarani/springmultitenant/interceptor/TenantContext.java
+++ src/main/java/info/juangnakarani/springmultitenant/interceptor/TenantContext.java
@@ -1,7 +1,12 @@
 package info.juangnakarani.springmultitenant.interceptor;
 
 import info.juangnakarani.springmultitenant.pojo.Tenant;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class TenantContext {
     private static final ThreadLocal<String> currentTenant = new InheritableThreadLocal<>();
 
